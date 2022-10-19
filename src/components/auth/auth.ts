0d281163cfdef66ddc036bf4cbcb6ecb1e025c75
Anas Ouzoline <87352192+anasabbal@users.noreply.github.com>
2022-10-19 16:23:42 +0100

    add validation for register

--- src/components/auth/auth.ts
+++ src/components/auth/auth.ts
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 
 type Props = {};
@@ -20,6 +22,39 @@ export default class Auth extends Component{
             email: "",
             password : ""
         };
+    }
+    validation(){
+        return Yup.object().shape({
+            firstName : Yup.string().test(
+                "len",
+                "The firstname must be between 3 and 20 characters.",
+                (val: any) =>
+                    val &&
+                    val.toString().length >= 3 &&
+                    val.toString().length <= 20
+            ).required("This field is required!"),
+            lastName: Yup.string().test(
+                "len",
+                "The firstname must be between 3 and 20 characters.",
+                (val: any) =>
+                    val &&
+                    val.toString().length >= 3 &&
+                    val.toString().length <= 20
+            ).required("This field is required!"),
+            email: Yup.string()
+                .email("This is not a valid email.")
+                .required("This field is required!"),
+            password: Yup.string()
+                .test(
+                    "len",
+                    "The password must be between 6 and 40 characters.",
+                    (val: any) =>
+                        val &&
+                        val.toString().length >= 6 &&
+                        val.toString().length <= 40
+                    )
+                    .required("This field is required!"),
+        });
     }
     handleRegister(formValue: {firstName: string, lastName: string, email: string, password: string}){
         const {firstName, lastName, email, password} = formValue;
