ed40b85579383ba0014a71c68e5d5b8e576788f3
Anas Ouzoline <87352192+anasabbal@users.noreply.github.com>
2022-10-19 15:21:18 +0100

    begin service auth

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,5 +1,18 @@
 import { Component } from "react";
 
-class About extends Component{
+
+type Props = {};
+
+type State = {
+  content: string;
+}
+
+
+class About extends Component<Props, State>{
+    constructor(props: Props){
+        super(props);
+
+        this.state = {content: ""};
+    };
     
 }
